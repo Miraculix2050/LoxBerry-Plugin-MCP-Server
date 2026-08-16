0fe5a83a679742311638caeebd965557c3a4c736
Miraculix2050 <198097126+Miraculix2050@users.noreply.github.com>
2026-08-16 02:42:09 +0200

    Test emitted admin security headers

--- tests/perl_stubs/LoxBerry/System.pm
+++ tests/perl_stubs/LoxBerry/System.pm
@@ -4,6 +4,15 @@ use warnings;
 use Exporter 'import';
 our @EXPORT = qw($lbhomedir $lbpplugindir $lbpconfigdir $lbpdatadir $lbpbindir $lbptemplatedir $lbplogdir);
 our ($lbhomedir, $lbpplugindir, $lbpconfigdir, $lbpdatadir, $lbpbindir, $lbptemplatedir, $lbplogdir);
+BEGIN {
+    $lbhomedir = $ENV{LB_TEST_HOME} // '';
+    $lbpplugindir = $ENV{LB_TEST_PLUGIN_DIR} // 'mcpserver';
+    $lbpconfigdir = $ENV{LB_TEST_CONFIG_DIR} // '';
+    $lbpdatadir = $ENV{LB_TEST_DATA_DIR} // '';
+    $lbpbindir = $ENV{LB_TEST_BIN_DIR} // '';
+    $lbptemplatedir = $ENV{LB_TEST_TEMPLATE_DIR} // '';
+    $lbplogdir = $ENV{LB_TEST_LOG_DIR} // '';
+}
 sub pluginversion { return 'test'; }
 sub pluginloglevel { return 3; }
 sub read_file { return ''; }
